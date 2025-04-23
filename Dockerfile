a46db089a5b0efcd08091d718d83b59ad0ad8ef9
atthulurichetan <atthulurichetan@gmail.com>
2025-04-23 23:39:19 +0530

    done with nginx and dockerfile practices

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 8099
 
 # Define environment variable
-ENV NAME World
+ENV NAME="World"
 
 # Run app.py when the container launches
 CMD ["python", "main.py"]
--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-'''
-  This is the config file which includes all ccnfiguation info here
-'''
-
 import os
 
 _DB_CONF = {
- 'host': os.getenv('DB_HOST', 'student.chbcdne9jh1l.ap-south-1.rds.amazonaws.com'),
- 'port': int(os.getenv('DB_PORT', 3306)),
- 'user': os.getenv('DB_USER', 'admin'),
- 'passwd': os.getenv('DB_PASSWORD', 'Tosvert123#'),
- 'db': os.getenv('DB_NAME', 'student')
+ 'host': os.environ.get('DB_HOST', '10.0.2.15'),
+ 'port': 3306,
+ 'user': os.environ.get('DB_USER', 'satya'),
+ 'passwd': os.environ.get('DB_PASSWORD', 'Shiva@1192'),
+ 'db': os.environ.get('DB_NAME', 'student')
+#  'host':'localhost',
+#  'port':3306,
+#  'user':'satya',
+#  'passwd':'Shiva@1192',
+#  'db':'student'
 }
-
